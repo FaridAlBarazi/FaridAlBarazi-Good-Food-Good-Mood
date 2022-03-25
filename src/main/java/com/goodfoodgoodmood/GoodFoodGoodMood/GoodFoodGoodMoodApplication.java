bb6f6e456a4160d6c92bc0ae01c935145ebeb2c9
nathancia <nathancia@icloud.com>
2022-03-25 10:29:00 +0100

    Création du projet

---
+++ src/main/java/com/goodfoodgoodmood/GoodFoodGoodMood/GoodFoodGoodMoodApplication.java
@@ -0,0 +1,13 @@
+package com.goodfoodgoodmood.GoodFoodGoodMood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodFoodGoodMoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodFoodGoodMoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodfoodgoodmood/GoodFoodGoodMood/GoodFoodGoodMoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodfoodgoodmood.GoodFoodGoodMood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodFoodGoodMoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
