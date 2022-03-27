81d4f424494733cd73c2bc0d35c78761988d7b97
Farid AL BARAZI <farid33talence@hotmal.fr>
2022-03-27 14:02:54 +0200

    reglage probleme application.properties

--- src/main/java/com/goodfoodgoodmood/GoodFoodGoodMood/beans/Recettes.java
+++ src/main/java/com/goodfoodgoodmood/GoodFoodGoodMood/beans/Recettes.java
@@ -1,4 +1,22 @@
 package com.goodfoodgoodmood.GoodFoodGoodMood.beans;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Recettes {
+
+    // indique que ID est la clés primaire
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID ;
+    private String name;
+    private String ingredient;
+    private int durée;
+    private String Specialité;
+    private int Healthy;
+
+
 }
