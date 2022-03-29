5c01958f59b928c151f501b05d9bf48348024a89
Farid AL BARAZI <farid33talence@hotmal.fr>
2022-03-30 01:47:58 +0200

    changement mon form cote forumalaire de saisis

--- src/main/resources/static/js/recipeForm.js
+++ src/main/resources/static/js/recipeForm.js
@@ -2,42 +2,71 @@ let survey_options = document.getElementById('survey_options');
 let add_more_fields = document.getElementById('add_more_fields');
 let remove_fields = document.getElementById('remove_fields');
 
-let number=2
-add_more_fields.onclick = function(){
-number++
-        let newField1 = document.createElement('input');
-        newField1.setAttribute('type','text');
-        newField1.setAttribute('name','survey_options[]');
-        newField1.setAttribute('class','survey_options');
-        newField1.setAttribute('size',"50");
-        newField1.setAttribute('placeholder',' ingredient'+number );
-        survey_options.appendChild(newField1);
+$(document).ready(function () {
+    let arr = [
+        {val: "gramme", text: 'gramme'},
+        {val: "litre", text: 'litre'},
+        {val: "millilitre", text: 'millilitre'},
+        {val: "cuillère à soupe", text: 'cuillère à soupe'},
+        {val: "cuillère à café", text: 'cuillère à café'}
+    ];
 
-        let newField2 = document.createElement('input');
-        newField2.setAttribute('type','text');
-        newField2.setAttribute('name','survey_options[]');
-        newField2.setAttribute('class','survey_options');
-        newField2.setAttribute('size',"50");
-        newField2.setAttribute('placeholder','Another ingredient');
+    let sel = $('<select>').appendTo($("#Element primaire"));
+    $(arr).each(function () {
+        sel.append($("<option>").attr('value', this.val).text(this.text));
+    });
+});
 
-        survey_options.appendChild(newField2);
-        let newField3 = document.createElement('input');
-        newField3.setAttribute('type','text');
-        newField3.setAttribute('name','survey_options[]');
-        newField3.setAttribute('class','survey_options');
-        newField3.setAttribute('size',"50");
-        newField3.setAttribute('placeholder','Another ingredient');
-        survey_options.appendChild(newField3);
+let number = 1
+add_more_fields.onclick = function () {
+    number++
+    let newField1 = document.createElement('input');
+    newField1.setAttribute('id', 'ingredient_' + number);
+    newField1.setAttribute('type', 'text');
+    newField1.setAttribute('name', 'survey_options[]');
+    newField1.setAttribute('class', 'survey_options');
+    newField1.setAttribute('size', "50");
+    newField1.setAttribute('placeholder', ' ingredient ' + number);
+    survey_options.appendChild(newField1);
 
+    let newField2 = document.createElement('input');
+    newField1.setAttribute('id', 'quantité_' + number);
+    newField2.setAttribute('type', 'text');
+    newField2.setAttribute('name', 'survey_options[]');
+    newField2.setAttribute('class', 'survey_options');
+    newField2.setAttribute('size', "50");
+    newField2.setAttribute('placeholder', 'quantité ' + number);
+    survey_options.appendChild(newField2);
+
+    let arr = [
+        {val: "gramme", text: 'gramme'},
+        {val: "litre", text: 'litre'},
+        {val: "millilitre", text: 'millilitre'},
+        {val: "cuillère à soupe", text: 'cuillère à soupe'},
+        {val: "cuillère à café", text: 'cuillère à café'}
+    ];
+    let sel = $('<select>').appendTo($("#survey_options"));
+    $(arr).each(function () {
+        sel.append($("<option>").attr('value', this.val).text(this.text));
+    })
 
 
 }
 
-remove_fields.onclick = function(){
+remove_fields.onclick = function () {
+
     let input_tags = survey_options.getElementsByTagName('input');
-    if(input_tags.length > 1) {
+
+    if (input_tags.length > 1) {
         survey_options.removeChild(input_tags[(input_tags.length) - 1]);
         survey_options.removeChild(input_tags[(input_tags.length) - 1]);
-        survey_options.removeChild(input_tags[(input_tags.length) - 1]);
+
+        // delete the select part of the form
+        del(document.querySelector("#survey_options > select"))
+
+
+
+        number--
+
     }
 }
