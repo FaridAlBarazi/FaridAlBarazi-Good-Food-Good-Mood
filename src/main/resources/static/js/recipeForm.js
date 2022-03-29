648a59127ebf600c0a164a0f8d364ffe44058abc
Farid AL BARAZI <farid33talence@hotmal.fr>
2022-03-29 15:50:54 +0200

    changement mon form cote forumalaire de saisis

--- src/main/resources/static/js/recipeForm.js
+++ src/main/resources/static/js/recipeForm.js
@@ -2,19 +2,42 @@ let survey_options = document.getElementById('survey_options');
 let add_more_fields = document.getElementById('add_more_fields');
 let remove_fields = document.getElementById('remove_fields');
 
+let number=2
 add_more_fields.onclick = function(){
-    let newField = document.createElement('input');
-    newField.setAttribute('type','text');
-    newField.setAttribute('name','survey_options[]');
-    newField.setAttribute('class','survey_options');
-    newField.setAttribute('siz',50);
-    newField.setAttribute('placeholder','Another ingredient');
-    survey_options.appendChild(newField);
+number++
+        let newField1 = document.createElement('input');
+        newField1.setAttribute('type','text');
+        newField1.setAttribute('name','survey_options[]');
+        newField1.setAttribute('class','survey_options');
+        newField1.setAttribute('size',"50");
+        newField1.setAttribute('placeholder',' ingredient'+number );
+        survey_options.appendChild(newField1);
+
+        let newField2 = document.createElement('input');
+        newField2.setAttribute('type','text');
+        newField2.setAttribute('name','survey_options[]');
+        newField2.setAttribute('class','survey_options');
+        newField2.setAttribute('size',"50");
+        newField2.setAttribute('placeholder','Another ingredient');
+
+        survey_options.appendChild(newField2);
+        let newField3 = document.createElement('input');
+        newField3.setAttribute('type','text');
+        newField3.setAttribute('name','survey_options[]');
+        newField3.setAttribute('class','survey_options');
+        newField3.setAttribute('size',"50");
+        newField3.setAttribute('placeholder','Another ingredient');
+        survey_options.appendChild(newField3);
+
+
+
 }
 
 remove_fields.onclick = function(){
     let input_tags = survey_options.getElementsByTagName('input');
-    if(input_tags.length > 3) {
+    if(input_tags.length > 1) {
+        survey_options.removeChild(input_tags[(input_tags.length) - 1]);
+        survey_options.removeChild(input_tags[(input_tags.length) - 1]);
         survey_options.removeChild(input_tags[(input_tags.length) - 1]);
     }
 }
