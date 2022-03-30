a965faa06b7a8bdbb586392ad69d7c8cf7f41e5c
nathancia <nathancia@icloud.com>
2022-03-30 13:07:34 +0200

    formulaire recette début

--- src/main/resources/static/js/recetteFormulaire.js
+++ src/main/resources/static/js/recetteFormulaire.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-
-    for (let i = 1; i < 5; i++) {
+    for (let i = 1; i < 3; i++) {
         $('#divIngredient').append(
             $(document.createElement('input')).prop({
                 type: 'text',
@@ -23,8 +22,7 @@ $(document).ready(function () {
             {val : "cuillère à soupe", text: 'cuillère à soupe'},
             {val : "cuillère à café", text: 'cuillère à café'}
         ];
-
-        let sel = $('<select>').appendTo($("#divIngredient"));
+        let sel = $("<select id='selectUnit" +i + "'>").appendTo($("#divIngredient"));
         $(arr).each(function() {
             sel.append($("<option>").attr('value',this.val).text(this.text));
         });
@@ -32,7 +30,7 @@ $(document).ready(function () {
     }
 });
 
-let nombre = 4;
+let nombre = 2;
 $("#buttonPlus").click(() => {
     nombre++;
     $('#divIngredient').append(
@@ -59,9 +57,61 @@ $("#buttonPlus").click(() => {
         {val : "cuillère à café", text: 'cuillère à café'}
     ];
 
-    let sel = $('<select>').appendTo($("#divIngredient"));
+    let sel = $("<select id='selectUnit" + nombre + "'>").appendTo($("#divIngredient"));
     $(arr).each(function() {
         sel.append($("<option>").attr('value',this.val).text(this.text));
     });
 
 })
+
+$("#envoieRecette").click(() => {
+let ingredient = [];
+    for(let i=1; i <= nombre;i++){
+        let ingr =
+        {
+            nom: $("#ingredient" + i).val(),
+            unit : $('#selectUnit'+i).find(":selected").text(),
+            quantite : $("#quantite" + i).val()
+        }
+        ingredient.push(ingr);
+    }
+
+    /*let ingredient = [
+        {
+            nom: "tomate",
+            unit: "aucune",
+            quantite: 2
+        },
+        {
+            nom: "lait",
+            unit: "ml",
+            quantite: 500
+        }
+    ]
+
+    let recette = {
+        name: "a",
+        duree: 1,
+        preparation: "a aa a",
+        specialite: "a",
+        ingredients : ingredient
+    };*/
+
+
+    let recette = {};
+    recette.name=$("#nomRecette").val();
+    recette.duree=$("#dureRecette").val();
+    recette.preparation=$("#floating-Textarea").val();
+    recette.specialite=$("#specialiteRecette").val();
+    recette.ingredients = ingredient;
+    console.log(JSON.stringify(recette));
+    $.ajax({
+        type: "POST",
+        headers: {"Content-Type": "application/json"},
+        url: "http://localhost:8080/API/recuperationRecette",
+        data: JSON.stringify(recette),
+        success: (retour) => {
+            alert(retour);
+        }
+    });
+});
