5b7580ece709c68107ef69444cd0acb064957c15
Farid AL BARAZI <farid33talence@hotmal.fr>
2022-04-01 10:23:59 +0200

    corectif message lien réseau

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -347,3 +347,30 @@ $("#closePopupCookies").click(() => {
     $("#verificationCookies").css("display", "none");
 })
 
+// partie lien reseau
+let whatsapp = document.querySelector(".whatsapp")
+let telegram = document.querySelector(".telegram")
+let twitter = document.querySelector(".twitter")
+
+let pageUrl = location.href
+// console.log(pageUrl)
+let message="Enjoy this awesome recipe application enjoy Good Food Good Mood"
+
+const whatsappApi='https://wa.me/?text='+pageUrl+message;
+const telegramApi="https://t.me/share/url?url="+pageUrl+message;
+const twitterApi=" https://twitter.com/intent/tweet?text="+pageUrl+message;
+
+whatsapp.addEventListener('click', ()=> {
+    // console.log('whatsapp button clicked')
+    window.open(url=whatsappApi,target="blank")
+})
+telegram.addEventListener('click', ()=> {
+    // console.log('telegram button clicked')
+    window.open(url=telegramApi,target="blank")
+
+})
+twitter.addEventListener('click', ()=> {
+    // console.log('twitter button clicked')
+    window.open(url=twitterApi,target="blank")
+
+})
