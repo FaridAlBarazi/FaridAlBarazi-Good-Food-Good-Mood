4521ba4a8e5581d0dfda39c65cd72c2323fc049a
nathancia <nathancia@icloud.com>
2022-04-02 16:32:45 +0200

    [Update] changement visuel accueil

--- src/main/resources/static/js/rechercheRecette.js
+++ src/main/resources/static/js/rechercheRecette.js
@@ -1,3 +1,47 @@
+// Récupération du cookie
+function getCookie(name) {
+    let dc = document.cookie;
+    let prefix = name + "=";
+    let begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    } else {
+        begin += 2;
+        let end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+            end = dc.length;
+        }
+    }
+    return decodeURI(dc.substring(begin + prefix.length));
+}
+
+if (getCookie("pseudo") == null) {
+    $("#recette").css("display", "none");
+    $("#boutonAvis").css("display", "none");
+} else {
+    $("#connexionButton").css("display", "none");
+    $("#dropdown").css("display", "block");
+    $("#pseudo").html(getCookie("pseudo"));
+    $("#recette").css("display", "block");
+    $("#boutonAvis").css("display", "block");
+}
+
+// deconnexion
+
+$("#deconnexion").click(() => {
+    $.get("http://localhost:8080/API/deconnexion", () => {
+        $("#dropdown").css("display", "none")
+        $("#connexionButton").css("display", "block");
+        $("#pseudo").html("");
+        $("#pseudoConnexion").val('');
+        $("#passwordConnexion").val('');
+        $("#recette").css("display", "none");
+        $("#boutonAvis").css("display", "none");
+        document.location.href = "index.html";
+    })
+})
+
 function selectingredients() {
     $.ajax({
         type: "GET",
@@ -43,8 +87,9 @@ function selectspecialite() {
 let myselect1 = document.getElementById("myselect1");
 
 myselect1.addEventListener("change", function () {
-    console.log(myselect1.value)
-    $("#selectspecialiteingr").remove();
+    console.log(myselect1.value);
+    $("#resultat").empty();
+    //$("#selectspecialiteingr").remove();
     console.log(myselect2.value)
     if(myselect1.value == "Recettes utilisateurs" && myselect2.value == "Par ingredient"){
         selectingredients();
